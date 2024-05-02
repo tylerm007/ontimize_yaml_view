e656ad246da15bb745277f25f97575468a6fb3df
Tyler Band <tylerm007@gmail.com>
2024-05-02 17:39:09 -0400

    add reports and charts

--- ui/yaml/src/app/main/Entity/home/Entity-home.component.ts
+++ ui/yaml/src/app/main/Entity/home/Entity-home.component.ts
@@ -1,5 +1,7 @@
 import { AfterViewInit, Component, ViewChild } from '@angular/core';
 import { OTableButtonComponent, OTableComponent } from 'ontimize-web-ngx';
+import { OChartModule } from 'ontimize-web-ngx-charts';
+import {OReportModule,OReportStoreService} from 'ontimize-web-ngx-report'
 
 @Component({
   selector: 'Entity-home',
@@ -14,8 +16,8 @@ export class EntityHomeComponent implements AfterViewInit {
   protected button: OTableButtonComponent;
 
   ngAfterViewInit() {
-   // this.button.onClick.subscribe(event => {
-   //   this.reportStoreService.openFillReport("94fa9d2a-e9cc-458a-a680-9bc576e14a38", {}, {});
+    //this.button.onClick.subscribe(event => {
+   //   this.reportStoreService.openFillReport("94fa9d2a-e9cc-458a-a680-9bc576e14a38");
    // });
   }
 
