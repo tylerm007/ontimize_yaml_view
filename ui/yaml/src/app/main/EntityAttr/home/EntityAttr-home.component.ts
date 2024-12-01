ac7d2d9d7b563e5f81ff1e64a73b088ece5bd659
Tyler Band <tylerm007@gmail.com>
2024-12-01 10:03:41 -0500

    fix grants and rbacs

--- ui/yaml/src/app/main/EntityAttr/home/EntityAttr-home.component.ts
+++ ui/yaml/src/app/main/EntityAttr/home/EntityAttr-home.component.ts
@@ -1,5 +1,5 @@
 import { AfterViewInit, Component, ViewChild } from '@angular/core';
-import { OTableButtonComponent, OTableComponent } from 'ontimize-web-ngx';
+import { OTableButtonComponent, OTableComponent, OTranslateModule } from 'ontimize-web-ngx';
 import { OChartModule } from 'ontimize-web-ngx-charts';
 import {OReportModule,OReportStoreService} from 'ontimize-web-ngx-report'
 
