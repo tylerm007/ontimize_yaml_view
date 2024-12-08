2729534de44298e486d34e91f5cc16250ae1849c
Tyler Band <tylerm007@gmail.com>
2024-12-08 08:03:12 -0500

    app create button start

--- rebuild_page.sh
+++ rebuild_page.sh
@@ -7,4 +7,6 @@ echo 'change dir to $1'
 cd $1
 cd ../..
 echo 'rebuild app: $2 for api-endpoint $3'
+# if $3 empty -then we skip --api-endpoint
+# if $2 does not exist - we do an app-create first then app-build
 als app-build --app=$2 --api-endpoint=$3
